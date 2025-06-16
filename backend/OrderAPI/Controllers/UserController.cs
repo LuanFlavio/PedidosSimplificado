6060343201fd54cb98f19beae7230c488d4fb52f
LuanFlavio <luanflavio60@gmail.com>
2025-06-15 22:24:06 -0300

    refact: restruturando repo para suportar backend e frontend

---
+++ backend/OrderAPI/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Application.DTOs;
+using Application.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace OrderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUser user;
+
+        public UserController(IUser user)
+        {
+            this.user = user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginResponse>> LoginUser(LoginDTO loginDTO)
+        {
+            var result = await user.LoginUserAsync(loginDTO);
+
+            if (result.Flag)
+                return Ok(result);
+            else return BadRequest(result);
+        }
+        
+        [HttpPost("register")]
+        public async Task<ActionResult<RegistrationResponse>> RegisterUser(RegisterUserDTO registerDTO)
+        {
+            var result = await user.RegistrationUserAsync(registerDTO);
+
+            if(result.Flag)
+                return Ok(result);
+            else return BadRequest(result);
+
+        }
+
+        [Authorize]
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<LoginResponse>> LoginUser(string userId)
+        {
+            var result = await user.GetUserAsync(userId);
+
+            if (result.Flag)
+                return Ok(result.User);
+            else return NotFound(result.Message);
+        }
+    }
+}
